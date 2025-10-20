40c8fa98a65d9a27048276f083624966062372cd
Florian Klink <flokli@flokli.de>
2025-10-20 15:38:29 +0300

    bencher: init at 0.5.6
    See #390884.

---
+++ pkgs/by-name/be/bencher/package.nix
@@ -0,0 +1,69 @@
+{
+  lib,
+  stdenv,
+
+  fetchFromGitHub,
+  rustPlatform,
+
+  fontconfig,
+  mold,
+  pkg-config,
+}:
+
+rustPlatform.buildRustPackage (finalAttrs: {
+  pname = "bencher";
+  version = "0.5.6";
+
+  src = fetchFromGitHub {
+    owner = "bencherdev";
+    repo = "bencher";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-iv0BScnDYVtkMnvGv3JysamuOANRNpvY8+ZC32aa2iA=";
+  };
+
+  cargoHash = "sha256-9Uf2XvBjZUudrfrLQCu4lCsGLx1zUz95nkNrMHTekm8=";
+
+  cargoBuildFlags = lib.cli.toGNUCommandLine { } {
+    package = [
+      "bencher_api"
+      "bencher_cli"
+    ];
+  };
+
+  # does dlopen() libfontconfig during tests and at runtime
+  RUSTFLAGS = lib.optionalString stdenv.targetPlatform.isElf "-C link-arg=-Wl,--add-needed,${fontconfig.lib}/lib/libfontconfig.so";
+
+  nativeBuildInputs = [
+    # .cargo/config.toml selects mold
+    mold
+    pkg-config
+  ];
+
+  buildInputs = [
+    fontconfig
+  ];
+
+  checkFlags = [
+    "--skip=licensor::test::test_bencher_cloud_annual"
+    "--skip=licensor::test::test_bencher_cloud_monthly"
+  ];
+
+  postInstall = ''
+    mv $out/bin/api $out/bin/bencher-api
+  '';
+
+  meta = {
+    description = "Suite of continuous benchmarking tools";
+    homepage = "https://bencher.dev";
+    changelog = "https://github.com/bencherdev/bencher/releases/tag/v${finalAttrs.version}";
+    license = [
+      lib.licenses.asl20
+      lib.licenses.mit
+    ];
+    maintainers = with lib.maintainers; [
+      flokli
+    ];
+    platforms = lib.platforms.unix;
+    mainProgram = "bencher";
+  };
+})
