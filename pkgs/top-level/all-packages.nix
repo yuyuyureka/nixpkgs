b11d4e7917947aec13c8d4c4213e11d490e48f1a
Yureka <yureka@cyberchaos.dev>
2026-02-27 09:58:02 +0100

    firefoxpwa: wrap using wrapFirefox

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9579,6 +9579,8 @@ with pkgs;
     pname = "firefox-bin";
   };
 
+  firefoxpwa = wrapFirefox firefoxpwa-unwrapped { };
+
   librewolf = wrapFirefox librewolf-unwrapped {
     inherit (librewolf-unwrapped) extraPrefsFiles extraPoliciesFiles;
     libName = "librewolf";
